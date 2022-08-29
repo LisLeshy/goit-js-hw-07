0b9368e10029380eb51f24f9abe194b37f63a43d
Oleksii Vykhryst <19leshy77@gmail.com>
2022-08-29 12:43:27 +0300

    исправил ошибки

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -27,22 +27,26 @@ function makeGallery(galleryItems) {
 
 function openModal(event) {
   event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+  console.log(event.target.classList.contains("gallery__image"));
+
   const instance = basicLightbox.create(`
     <img src="${event.target.dataset.source}" width="800" height="600" >
 	`);
   instance.show();
   function onEscKeyPress(e) {
-    if (!basicLightbox.visible()) {
-      return;
-    }
-    console.log(basicLightbox);
-
     if (e.code === "Escape") {
       instance.close();
       window.removeEventListener("keydown", onEscKeyPress);
+    } else {
+      instance.close();
+      containerForGallery.removeEventListener("click", openModal);
     }
     console.log(e.code);
   }
+
   window.addEventListener("keydown", onEscKeyPress);
 }
 containerForGallery.addEventListener("click", openModal);
